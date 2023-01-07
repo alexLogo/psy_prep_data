86d6da89663a7a95ab597593517baadee1625ceb
Alexander Logovinsky <44161777+alexLogo@users.noreply.github.com>
2023-01-08 00:02:29 +0200

    Update read_answers.py
    Update read_answers.py

--- Raw_Data/answers/read_answers.py
+++ Raw_Data/answers/read_answers.py
@@ -2,41 +2,19 @@ import pandas as pd
 import numpy as np
 from os import listdir
 
-from Raw_Data.utils.utils import number_to_string
 import Raw_Data.configurations as cfg
 import pathes
-
-def path_resolver(subject_num):
-    path = pathes.raw_data_path + cfg.participant_dir_name + number_to_string(subject_num) + '/' + cfg.answers_file_name
-    return path
-
-
-
-def filter_answer_file_question(df):
-    # take only real trials rows
-    # filter by question
-    df = df[df[cfg.answer_question_col_name] == cfg.relevant_question]
-    
-    
-    # take only id and relevant columns
-    df = df[[cfg.answers_index] + cfg.answer_relevant_cols]
-    
-    return df
-    
-def label_answer_file_one_columns(df, col_id, tranfrom_dic=cfg.trial_labels_dic):
-    df.iloc[:,col_id].replace(tranfrom_dic, inplace=True)
-    return df
-
+from Raw_Data.utils import utils
 
 def read_answers(subject_num):
     # resolve path of trials file
-    path = path_resolver(subject_num)
+    path = utils.path_resolver(subject_num, cfg.answers_file_name)
     
     # read trials data
     data = pd.read_csv(path)
     
     # filter trials data
-    data = filter_answer_file_question(data)
+    data = utils.filter_answer_file_question(data)
     
     # reset index
     data.reset_index(inplace=True, drop=True)
